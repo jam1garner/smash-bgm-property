03249630954194062b10ebcc3456860a697abb93
jam1garner <8260240+jam1garner@users.noreply.github.com>
2020-11-03 02:01:25 -0500

    Add CLI for converting to and from yaml

--- src/lib.rs
+++ src/lib.rs
@@ -20,15 +20,22 @@
 /// # }
 /// ```
 
-use binread::{BinRead, BinReaderExt, derive_binread, BinResult};
+use binread::{BinRead, BinReaderExt, derive_binread};
 use binwrite::{BinWrite, WriterOption};
 
 use std::fs::File;
 use std::path::Path;
 use std::io::{self, Write, BufReader, BufWriter};
 
+#[cfg(feature = "derive_serde")]
+use serde::{Serialize, Deserialize};
+
+mod hash40;
+
 /// Type alias for Hash40
 pub type Hash40 = u64;
+
+pub use binread::{BinResult as Result, Error};
 
 /// ```rust
 /// # fn main() -> binread::BinResult<()> {
@@ -49,6 +56,7 @@ pub type Hash40 = u64;
 /// # }
 /// ```
 #[derive_binread]
+#[cfg_attr(feature = "derive_serde", derive(Serialize, Deserialize))]
 #[derive(Debug)]
 #[br(magic = b"PMGB")]
 pub struct BgmPropertyFile (
@@ -70,8 +78,10 @@ impl BinWrite for BgmPropertyFile {
 }
 
 /// An entry representing a single nus3audio background music file
+#[cfg_attr(feature = "derive_serde", derive(Serialize, Deserialize))]
 #[derive(BinRead, BinWrite, Debug)]
 pub struct Entry {
+    #[serde(with = "serde_hash40")]
     pub name_id: Hash40,
     pub unk: u32,
     pub loop_start_sample: u32,
@@ -84,17 +94,78 @@ pub struct Entry {
     pub total_samples: u32,
 }
 
+#[cfg(feature = "derive_serde")]
+pub fn set_labels<P: AsRef<Path>>(path: P) -> Result<()> {
+    fn inner(path: &Path) -> Result<()> {
+        let contents = std::fs::read_to_string(path)?;
+        let labels = contents.split("\n")
+            .map(|string| (hash40::hash40(string.trim()), string.to_owned()))
+            .collect();
+
+        *serde_hash40::LABELS.lock().unwrap() = labels;
+
+        Ok(())
+    }
+
+    inner(path.as_ref())
+}
+
+#[cfg(feature = "derive_serde")]
+mod serde_hash40 {
+    use std::{
+        sync::Mutex,
+        collections::HashMap,
+    };
+
+    lazy_static::lazy_static! {
+        pub static ref LABELS: Mutex<HashMap<Hash40, String>> = Mutex::new(HashMap::new());
+    }
+
+    use super::{hash40::hash40, Hash40};
+    use serde::{Deserialize, Deserializer, Serializer};
+
+    pub fn deserialize<'de, D, E>(deserializer: D) -> Result<u64, D::Error>
+    where
+        D: Deserializer<'de, Error = E>,
+        E: serde::de::Error,
+    {
+        let s: String = Deserialize::deserialize(deserializer)?;
+
+        if s.starts_with("0x") {
+            u64::from_str_radix(s.trim_start_matches("0x"), 16)
+                .map_err(|_| D::Error::custom(format!("{} is an invalid Hash40", s)))
+        } else {
+            Ok(hash40(&s))
+        }
+    }
+
+    pub fn serialize<S>(hash40: &Hash40, serializer: S) -> Result<S::Ok, S::Error> 
+        where S: Serializer,
+    {
+        match LABELS.lock().unwrap().get(hash40) {
+            Some(label) => {
+                serializer.serialize_str(&label)
+            }
+            None => {
+                serializer.serialize_str(&format!("{:#x}", hash40))
+            }
+        }
+    }
+}
+
+
 impl BgmPropertyFile {
-    pub fn open<P: AsRef<Path>>(path: P) -> BinResult<Self> {
+    pub fn open<P: AsRef<Path>>(path: P) -> Result<Self> {
         BufReader::new(File::open(path)?).read_le()
     }
 
-    pub fn save<P: AsRef<Path>>(&self, path: P) -> io::Result<()> {
+    pub fn save<P: AsRef<Path>>(&self, path: P) -> Result<()> {
         self.write(&mut BufWriter::new(File::create(path)?))
     }
 
-    pub fn write<W: Write>(&self, writer: &mut W) -> io::Result<()> {
+    pub fn write<W: Write>(&self, writer: &mut W) -> Result<()> {
         self.write_options(writer, &binwrite::writer_option_new!(endian: binwrite::Endian::Little))
+            .map_err(Into::into)
     }
 
     pub fn new(entries: Vec<Entry>) -> Self {
